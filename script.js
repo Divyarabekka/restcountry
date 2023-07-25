27377f48054feadf0899c2ab5a1d15e44e1d7874
Divyarabekka <137371881+Divyarabekka@users.noreply.github.com>
2023-07-25 12:18:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ res.then((data)=>data.json()).then((data1)=>{
         <div class="card" style="width: 18rem;">
         <img src="${data1[i].flag}" class="card-img-top" alt="..." width="500px" height="200px">
         <div class="card-body">
-          <h5 class="card-title">${data1[i].name}</h5>
-          <h6 class="card-subtitle mb-2 text-muted">${data1[i].region}</h6>
-          <p class="card-text"> ${data1[i].subregion}</p>
+          <h5 class="card-title"> Name: ${data1[i].name}</h5>
+          <h6 class="card-subtitle mb-2 text-muted"> Region: ${data1[i].region}</h6>
+          <p class="card-text"> Subregion: ${data1[i].subregion}</p>
         </div></div></div>
       </div> `          
         document.body.append(div);
